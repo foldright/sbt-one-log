b300feacad0067da81aba91ab101f10d7bd55766
Jerry Lee <oldratlee@gmail.com>
2021-07-15 20:25:45 +0800

    ! improve bump-version.sh

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -1,8 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+set -eEuo pipefail
 
-newVersion=`perl -npe "s/version in ThisBuild\s+:=\s+\"(.*)\"/\1/" version.sbt | sed -e "/^$/d"`
+# change work dir to project root
+cd "$(dirname "$(readlink -f "$0")")"/.. || {
+  echo "Fail to change work dir!" 1>&2
+  exit 1
+}
 
-for f in $(/bin/ls src/sbt-test/sbt-one-log/*/project/plugins.sbt); do
-  echo $f;
-  perl -i -npe "s/addSbtPlugin\(\"com.zavakid.sbt\".*/addSbtPlugin\(\"com.zavakid.sbt\" % \"sbt-one-log\" % \"$newVersion\"\)/" $f; \
-done;
+if [ $# != 1 ]; then
+  {
+    echo "Only 1 argument for version!"
+    echo
+    echo "usage: $0 <new version>"
+  } 1>&2
+
+  exit 1
+fi
+
+readonly version="$1"
+
+# update version for version.sbt file
+
+readonly version_sbt_file=version.sbt
+
+echo "update $version_sbt_file to version $version"
+sed -ri 's/(version\s+in\s+ThisBuild\s+:=\s+)(".*)(".*)/\1"'"$version"'\3/' "$version_sbt_file"
+
+
+# update version for plugins.sbt files
+
+for f in src/sbt-test/sbt-one-log/*/project/plugins.sbt; do
+  echo "update $f to version $version"
+
+  sed -ri 's/addSbtPlugin\s*\("com.zavakid.sbt"\s*%\s*"(.*)"\s*%\s*"(.*)(".*)/addSbtPlugin\("com.zavakid.sbt" % "\1" % "'"$version"'\3/' "$f"
+done
